22fc142a9ef1ce89240f93574b131a780b876e5a
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-03-16 18:46:46 -0300

    Manipulando a classe Banco

--- src/main/kotlin/one/dio/digionebank/TesteBanco.kt
+++ src/main/kotlin/one/dio/digionebank/TesteBanco.kt
@@ -1,5 +1,12 @@
 package one.dio.digionebank
 
 fun main() {
+    val digiOneBank = Banco("Banco Joias", numero = 6)
 
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    // Criando uma cópia do objeto é possível alterar um valor imutável
+    val digiOneBank2 = digiOneBank.copy(nome = "Banco Vingadores")
+    println(digiOneBank2)
 }
