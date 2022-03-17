c4f20186d339ce07cd47fed03f5a0e57f585d3f6
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-03-17 19:31:18 -0300

    Manipulando classe funcionario e criando e contando por grupos

---
+++ src/main/kotlin/one/dio/collections/TesteList.kt
@@ -0,0 +1,46 @@
+package one.dio.collections
+
+fun main() {
+    val saitama = Funcionario(nome = "Saitama", salario = 5000.0, contratacao = "CLT")
+    val goku = Funcionario(nome = "Goku", salario = 4000.0, contratacao = "PJ")
+    val seiya = Funcionario(nome = "Seiya", salario = 500.0, contratacao = "CLT")
+
+    val funcionarios = listOf(saitama, goku, seiya)
+    funcionarios.forEach { println(it) }
+
+    println("------------------")
+    // retorna o funcionario Seiya
+    println(funcionarios.find { it.nome == "Seiya" })
+
+    println("------------------")
+    // Ordenar a lista funcionarios por salário
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach { println(it) }
+
+    println("------------------")
+    // Agrupar por grupos
+    funcionarios
+        .groupBy { it.contratacao }
+        .forEach { println(it) }
+
+    println("----------------")
+    // Quantidade de grupos por contratação
+    println(funcionarios.groupBy { it.contratacao }.count())
+
+    // Quantidade de cada grupo de contratação
+    println(funcionarios.groupingBy { it.contratacao }.eachCount())
+
+}
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val contratacao: String
+){
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salário: $salario
+            
+        """.trimIndent()
+}
